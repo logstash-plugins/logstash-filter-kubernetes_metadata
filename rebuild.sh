6f8b733846feb06711a876f7064465c0e767abe2
Philip Hutchins <flipture@gmail.com>
2017-04-26 15:49:42 -0400

    Bumping version
    Fixes #1

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 logstash-plugin remove logstash-filter-kubernetes_metadata;
-rm ./logstash-filter-kubernetes_metadata-1.0.1.gem;
+rm ./logstash-filter-kubernetes_metadata-1.0.3.gem;
 gem build logstash-filter-kubernetes_metadata.gemspec;
-logstash-plugin install ./logstash-filter-kubernetes_metadata-1.0.1.gem
+logstash-plugin install ./logstash-filter-kubernetes_metadata-1.0.3.gem
